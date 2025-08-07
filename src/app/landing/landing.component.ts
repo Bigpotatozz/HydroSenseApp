fcf0e61f1bec5667fdb6ffd9d32dcd0f721cf8a4
oscar <oscarmancilla227@gmail.com>
2025-08-07 13:40:12 -0600

    comentarios traidos desde api

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,23 +1,55 @@
-import { Component } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { Toolbar } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
-import { InicioComponent } from "./inicio/inicio.component";
-import { NosotrosComponent } from "./nosotros/nosotros.component";
-import { PreguntasComponent } from "./preguntas/preguntas.component";
-import { ProductosComponent } from "./productos/productos.component";
-import { ContactoComponent } from "./contacto/contacto.component";
-import { OpinionComponent } from "./opinion/opinion.component";
-import { CotizacionComponent } from "./cotizacion/cotizacion.component";
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { PreguntasComponent } from './preguntas/preguntas.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { OpinionComponent } from './opinion/opinion.component';
+import { CotizacionComponent } from './cotizacion/cotizacion.component';
+import { ComentarioService } from '../services/comentario.service';
+import { Comentario } from '../models/comentario.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
-  imports: [Toolbar, AvatarModule, ButtonModule, RouterModule, InicioComponent, NosotrosComponent, PreguntasComponent, ProductosComponent, ContactoComponent, OpinionComponent, CotizacionComponent],
+  imports: [
+    Toolbar,
+    AvatarModule,
+    ButtonModule,
+    RouterModule,
+    InicioComponent,
+    NosotrosComponent,
+    PreguntasComponent,
+    ProductosComponent,
+    ContactoComponent,
+    OpinionComponent,
+    CotizacionComponent,
+    NgFor,
+  ],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.css',
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  comentarios: Comentario[] = [];
 
+  comentariosMostrar: Comentario[] = [];
+  constructor(private comentarioService: ComentarioService) {}
+
+  ngOnInit() {
+    this.comentarioService.obtenerComentarios().subscribe({
+      next: (data) => {
+        this.comentarios = data.data;
+
+        this.comentariosMostrar = this.comentarios.slice(0, 3);
+      },
+      error: (error) => {
+        console.error('Error al obtener comentarios:', error);
+      },
+    });
+  }
 }
---
+++ src/app/models/comentario.model.ts
@@ -0,0 +1,6 @@
+export class Comentario {
+    constructor(public nombreUsuario: string,
+        public nombreSistema: string,
+        public comentario: string
+    ){}
+}
